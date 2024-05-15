071c4393f021b805ff344a3808aa742741b56a03
FahmiSar <fahmi.sardar@ucalgary.ca>
2024-05-14 21:56:48 -0600

    used sessionStorage to try to fix the issue where manually refresh is needed to see the next page

--- global.js
+++ global.js
@@ -1,55 +1,66 @@
-      // FUNCTIONS
-      function updatePageContent(){
-        var hasVisitedValimar = localStorage.getItem("visitedValimar");
-        var hasVisitedOrdine = localStorage.getItem("vistedOrdine");
-        var hasVistedTestaRossa = localStorage.getItem("vistedTestaRossa");
-        var hasVisitedZector = localStorage.getItem("visitedZector");
-        var hasVisitedArgrion = localStorage.getItem("vistedArgrion");
-        var hasVisitedIshmelga = localStorage.getItem("visitedIshmelga");
-        var hasVisitedElPrado = localStorage.getItem("visitedElPrado");
+// EVENTS //
+// this event will run the moment all DOM content has been loaded
+document.addEventListener("DOMContentLoaded", function () {
+    var hasVisitedValimar = localStorage.getItem("visitedValimar");
+    var hasVisitedOrdine = localStorage.getItem("vistedOrdine");
+    var hasVistedTestaRossa = localStorage.getItem("vistedTestaRossa");
+    var hasVisitedZector = localStorage.getItem("visitedZector");
+    var hasVisitedArgrion = localStorage.getItem("vistedArgrion");
+    var hasVisitedIshmelga = localStorage.getItem("visitedIshmelga");
+    var hasVisitedElPrado = localStorage.getItem("visitedElPrado");
 
-        var ordineLink = document.querySelector('a[href = "HTML Files/About Ordine.html"]');
-        var testaRossaLink = document.querySelector('a[href = "HTML Files/About Testa Rossa.html"]');
-        var argreionLink = document.querySelector('a[href = "HTML Files/About Argreion.html"]');
-        var zectorLink = document.querySelector('a[href = "HTML Files/About Zector.html"]');
-        var elPradoLink = document.querySelector('a[href = "HTML Files/About El Prado.html"]');
-        var ishmelgaLink = document.querySelector('a[href = "HTML Files/About Ishmelga.html"]');
-        
-        var greatOneDiv = document.querySelector(".great-one");
+    var ordineLink = document.querySelector('a[href = "HTML Files/About Ordine.html"]');
+    var testaRossaLink = document.querySelector('a[href = "HTML Files/About Testa Rossa.html"]');
+    var argreionLink = document.querySelector('a[href = "HTML Files/About Argreion.html"]');
+    var zectorLink = document.querySelector('a[href = "HTML Files/About Zector.html"]');
+    var elPradoLink = document.querySelector('a[href = "HTML Files/About El Prado.html"]');
+    var ishmelgaLink = document.querySelector('a[href = "HTML Files/About Ishmelga.html"]');
 
-        if(hasVisitedValimar){
-          ordineLink.style.display = 'inline';
-        }
+    var greatOneDiv = document.querySelector(".great-one");
 
-        if(hasVisitedOrdine){
-          testaRossaLink.style.display = "inline";
-        }
+    if (hasVisitedValimar) {
+        ordineLink.style.display = "inline";
+        refreshPageOnce()
+    }
 
-        if(hasVistedTestaRossa){
-          zectorLink.style.display = "inline";
-        }
+    if (hasVisitedOrdine) {
+        testaRossaLink.style.display = "inline";
+        refreshPageOnce()
+    }
 
-        if(hasVisitedZector){
-          argreionLink.style.display = "inline";
-        }
+    if (hasVistedTestaRossa) {
+        zectorLink.style.display = "inline";
+        refreshPageOnce()
+    }
 
-        if(hasVisitedArgrion){
-          ishmelgaLink.style.display = "inline";
-        }
+    if (hasVisitedZector) {
+        argreionLink.style.display = "inline";
+        refreshPageOnce()
+    }
 
-        if(hasVisitedIshmelga){
-          elPradoLink.style.display = "inline";
-        }
+    if (hasVisitedArgrion) {
+        ishmelgaLink.style.display = "inline";
+        refreshPageOnce()
+    }
 
-        if(hasVisitedElPrado){
-          greatOneDiv.style.display = "inline";
-        }
+    if (hasVisitedIshmelga) {
+        elPradoLink.style.display = "inline";
+        refreshPageOnce()
+    }
 
-      }
+    if (hasVisitedElPrado) {
+        greatOneDiv.style.display = "inline";
+        refreshPageOnce()
+    }
+});
 
 
-      // EVENTS //
-      // this event will run the moment all DOM content has been loaded
-      document.addEventListener("DOMContentLoaded", function(){
-        updatePageContent();
-      });
+function refreshPageOnce(){
+    var hasRefreshed = sessionStorage.getItem("hasRefreshed");
+
+    if(!hasRefreshed){
+        sessionStorage.setItem("hasRefreshed", true);
+        location.reload();
+    }
+
+}
